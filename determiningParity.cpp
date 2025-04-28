260085a2809a60953c9ac422dbce78cb59759360
Tatyana <186121244+TatyanaRicketts@users.noreply.github.com>
2025-04-28 12:40:28 -0400

    Add files via upload

---
+++ determiningParity.cpp
@@ -0,0 +1,16 @@
+/*This program prompts a user to enter a positive integer, anc determines its parity*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int input;
+    cout<<"Please enter a positive integer for its parity: ";cin>>input;
+
+    if (input % 2 == 0)
+        cout<<input<<" is even"<<endl;
+    else
+        cout<<input<<" is odd"<<endl;
+
+    return 0;
+}
