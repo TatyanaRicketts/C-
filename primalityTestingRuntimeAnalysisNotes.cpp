c3d0a0a9edf90f7dd0550cbeee3c510d7c159596
Tatyana <186121244+TatyanaRicketts@users.noreply.github.com>
2025-06-04 04:35:57 -0400

    Create primalityTestingRuntimeAnalysisNotes.cpp

---
+++ primalityTestingRuntimeAnalysisNotes.cpp
@@ -0,0 +1,44 @@
+/*Primality Testing (3 Approaches) Runtime Analysis Notes*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//Primality Testing Version 1
+bool isPrimeV1(int num){
+    int countDivs;
+    countDivs = 0;
+    for(int i = 1; i <= num; i++){
+        if(num % i == 0)
+            countDivs++;
+    }
+    return (countDivs == 2);
+}
+// T1(n) = 1 + 1 + 5*n + 2
+// T1(n) = 5n + 4
+
+
+//Primality Testing Version 2
+bool isPrimeV2(int num){
+    int countDivs = 0;
+    for(int i = 1; i <= num / 2; i++){
+        if(num % i == 0)
+            countDivs++;
+    }
+    return (countDivs == 1);
+}
+// T2(n) = 1 + 1 + 6*(n/2) + 2
+// T2(n) =  6/2*n + 2
+// T2(n) = 3n + 4
+
+
+// Primality Testing Version 3
+bool isPrimeV3(int num){
+    int countDivs = 0;
+    for(int i = 1; i <= sqrt(num); i++){
+        if(num % i == 0)
+            countDivs++;
+    }
+    return (countDivs == 1);
+}
+// T3(n) = 1 + 1 + 6*sqrt(n) + 2
+// T3(n) = 6sqrt(n) + 4
