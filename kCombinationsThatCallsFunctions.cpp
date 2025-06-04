3b46f0603c705620ab209c2b38f3a9f741f8bcdf
Tatyana <186121244+TatyanaRicketts@users.noreply.github.com>
2025-06-04 11:50:39 -0400

    Create kCombinationsThatCallsFunctions.cpp

---
+++ kCombinationsThatCallsFunctions.cpp
@@ -0,0 +1,41 @@
+/*
+K-Combinations Problem That Calls Funations: Write a program that reads from the user two positive integers n, k (n >= k)
+and prints the value of n choose k.
+
+Formula: n!/(k!*(n-k)!)
+
+Example: Please enter n and k (k<= n): 
+5 3
+5 choose 3 is 10
+*/
+#include <iostream>
+using namespace std;
+
+int factorial(int num){
+        int factRes, i;
+        
+        factRes = 1;
+        for(i = 1; i <= num; i++)
+            factRes *= i;
+            
+        return factRes;
+        }
+
+int main(){
+    int n, k, nFact, kFact, n_kFact, k_comb;
+    
+    cout<<"Please enter n and k (n >= k):"<<endl;
+    cin>>n>>k;
+    
+    nFact = factorial(n);
+    
+    kFact = factorial(k); 
+        
+    n_kFact = factorial(n - k);
+   
+    k_comb = nFact / (kFact * n_kFact);
+    
+    cout<<n<<" choose "<<k<<" is "<<k_comb<<endl;
+        
+        return 0;
+    }
