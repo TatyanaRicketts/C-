081eb8ebdfd5d6d0cc99aadc7851a6d3e784469a
Tatyana <186121244+TatyanaRicketts@users.noreply.github.com>
2025-06-19 22:40:41 -0400

    Create arrayPracticeNoInput10to100UpdatedTo200.cpp

---
+++ arrayPracticeNoInput10to100UpdatedTo200.cpp
@@ -0,0 +1,21 @@
+//Array practice without any inputs. This returns numbers: 10, 20, 30, 40, 50, 60, 70, 80, 90, 200
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numbers[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};//array for 11 integers
+    
+    
+    //change the 11th number 200
+    numbers[9] = 200;
+    
+    for(int i = 0; i < 10; i++){
+        cout<<numbers[i];
+        if(i < 9){
+        cout<<", ";
+        }
+    }
+        
+   return 0;
+}
